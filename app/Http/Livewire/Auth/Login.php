d4aab669134a14297a5965db05d71656a000193d
Sandro Nilton <sandro91111@gmail.com>
2023-08-03 14:55:35 -0500

    save 2

--- app/Http/Livewire/Auth/Login.php
+++ app/Http/Livewire/Auth/Login.php
@@ -4,8 +4,30 @@ namespace App\Http\Livewire\Auth;
 
 use Livewire\Component;
 
+use App\Http\Requests\Auth\LoginRequest;
+
 class Login extends Component
 {
+    public $email;
+    public $password;
+    public $remember;
+
+    protected function rules(){
+        return (new LoginRequest)->rules();
+    }
+
+    protected $messages = [
+        'email.required' => 'El campo correo electrónico es obligatorio.',
+        'email.email' => 'El campo correo electrónico no es válido',
+        'password.required' => 'El campo contraseña es obligatorio.',
+    ];
+
+
+    public function updated($fields)
+    {
+        $this->validateOnly($fields);
+    }
+
     public function render()
     {
         return view('livewire.auth.login');
