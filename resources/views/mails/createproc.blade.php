0e9712a9349c38e38246d59a0fc305c2b687432d
Sandro Nilton <sandro91111@gmail.com>
2023-09-05 20:45:38 -0500

    git bitbucket

--- resources/views/mails/createproc.blade.php
+++ resources/views/mails/createproc.blade.php
@@ -6,7 +6,25 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<body>
-    <h1>Se creo su nuevo tramite con identificador: </h1>
+<style>
+  .body {
+    border: 1px solid #003a;
+    text-align: center;
+    padding: 20px;
+    content: center;
+  }
+</style>
+<body class="body">
+  <p>
+    Colegio de Arquitectos del Perú
+  </p>
+  <img src="https://limacap.org/wp-content/uploads/2021/12/logo-niubiz.jpg" height="300" width="300" style="margin: -15px">
+    <h1>Nueva Solicitud de </h1>
+  <p>
+    Le informamos que la solicitud con codigo de proyecto ::: con dirección ::: ha sido registrada.
+  </p>
+  <p>
+    Puede visualizar la solicitud en el siguiente link: :::::::::::::::::::
+  </p>
 </body>
 </html>
