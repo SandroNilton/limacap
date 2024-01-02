f82534ffd075200eebeaaf03242ee8111590dc41
Sandro Nilton <sandro91111@gmail.com>
2024-01-02 13:24:13 -0500

    change panel 5

--- resources/views/admin/procedures/index.blade.php
+++ resources/views/admin/procedures/index.blade.php
@@ -2,7 +2,7 @@
   <div class="w-full h-full">
     <div class="flex flex-col h-full gap-4">
       <div class="flex items-center justify-between">
-        <p class="text-md font-semibold text-[rgb(17,24,39)] text-opacity-100">Trámites, <span class="text-[#10B981]">{{ Auth::user()->area ? Auth::user()->area->name : "Admin" }}</span></p>
+        <p class="text-md font-semibold text-[rgb(17,24,39)] text-opacity-100">Trámites, <span class="text-[#10B981]">{{ Auth::user()->area ? Auth::user()->area->name : "Admin" }} {{auth()->user()->area_id}}</span></p>
       </div>
       <div class="grid grid-cols-1">
         <div class="bg-white bg-opacity-100 border-b border-opacity-100 rounded-md border-[rgb(229,231,235)] shadow p-4">
