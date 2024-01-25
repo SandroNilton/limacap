4111b70cd434ebc6121bbaf773cb926caf12c320
Sandro Nilton <sandro91111@gmail.com>
2024-01-24 19:06:05 -0500

    change 23

--- resources/views/livewire/app/procedures/show.blade.php
+++ resources/views/livewire/app/procedures/show.blade.php
@@ -7,10 +7,6 @@
             <h4 class=" text-md text-opacity-100 text-[rgb(17,24,39)] font-bold">DETALLE DEL TRAMITE</h4>
           </div>
           <span class="flex items-center gap-3 mb-3">
-            <ion-icon name="people-outline" class="text-lg" wire:ignore></ion-icon>
-            <span class="text-sm font-medium">Clase: @switch($procedure_data[0]->user->type) @case(1) Natural @break @case(2) Juridica  @break @case(3) Agremiado @break @case(10)  Administrativo @break @endswitch</span>
-          </span>
-          <span class="flex items-center gap-3 mb-3">
             <ion-icon name="person-outline" class="text-lg" wire:ignore></ion-icon>
             <span class="text-sm font-medium">Nombre: {{ $procedure_data[0]->user->name }}</span>
           </span>
@@ -32,6 +28,66 @@
           </span>
         </div>
       </div>
+      <div>
+        <div class="bg-white bg-opacity-100 border-b border-opacity-100 rounded-md border-[rgb(229,231,235)] shadow p-4">
+          <div class="flex flex-col grid-cols-1 text-sm columns-1 gap-x-3">
+            @forelse ($procedure_files as $procedure_file)
+              @if($procedure_file->state == "rechazado")
+                <div class="min-w-full border border-dashed border-[#cdd5de] rounded-[3px]">
+                  <div class="flex items-center flex-1 p-2 rounded-sm gap-x-3">
+                    <button wire:click="downloadFile('{{ $procedure_file->id }}', '{{ $procedure_file->name }}', '{{ $procedure_file->file }}')" class="flex justify-center items-center rounded-[3px] w-8 h-[51px] bg-[#0d8a72] text-white text-[18px] cursor-pointer">
+                      <ion-icon  wire:ignore name="download-outline"></ion-icon>
+                    </button>
+                    <div class="flex-1">
+                      {{ $procedure_data[0]->state }}
+                      <div class="text-[13px] w-44 truncate leading-5 text-[#414d6a]" title="{{ $procedure_file->name }}">{{ $procedure_file->name }}</div>
+                      @if ($procedure_data[0]->state == 'aprobado' || $procedure_data[0]->state == 'cancelado')
+                      @else
+                        <div class="flex gap-x-3">
+                            <div class="flex text-[13px] leading-4 mb-3">
+                                <form wire:submit.prevent="changeFile('{{ $procedure_file->id }}', '{{ $procedure_file->requirement_id }}', '{{ $procedure_file->name }}', '{{ $procedure_file->file }}')" enctype="multipart/form-data" class="flex w-full gap-x-2.5">
+                                    <div class="border border-dashed border-[#d9d9da] transition duration-300 flex flex-row rounded-[3px] hover:border-[#0d8a72] gap-x-2.5">
+                                        <div class="px-4 inline-flex items-center border-r border-[#d9d9da] bg-white">
+                                        <span class="text-[13px] text-[#414d6a]">Archivo</span>
+                                        </div>
+                                        <input type="file" wire:model="file_replace" id="file_replace" class="cursor-pointer w-full flex text-[13px] leading-4 text-center justify-center bg-white py-1.5 px-3.5 relative m-0 flex-auto duration-300 ease-in-out file:hidden focus:outline-none">
+                                    </div>
+                                    <button type="submit" class="bg-[#0d8a72] px-1 rounded-[3px] text-white text-[20px] py-1 inline-flex items-center">
+                                        <ion-icon wire:ignore name="refresh-outline"></ion-icon>
+                                    </button>
+                                </form>
+                              </div>
+
+                        </div>
+                      @endif
+                    </div>
+                  </div>
+                </div>
+              @else
+                <div class="min-w-full border border-dashed border-[#cdd5de] rounded-[3px]">
+                    <div class="flex items-center flex-1 p-2 rounded-sm gap-x-3">
+                      <button wire:click="downloadFile('{{ $procedure_file->id }}', '{{ $procedure_file->name }}', '{{ $procedure_file->file }}')" class="flex justify-center items-center rounded-[3px] w-8 h-[51px] bg-[#0d8a72] text-white text-[18px] cursor-pointer">
+                        <ion-icon  wire:ignore name="download-outline"></ion-icon>
+                      </button>
+                        <div class="flex-1">
+                            <div class="text-[13px] w-44 truncate leading-5 text-[#414d6a]" title="{{ $procedure_file->name }}">{{ $procedure_file->name }}</div>
+                            <div class="text-[13px] w-44 truncate leading-5 text-[#414d6a]" title="{{ $procedure_file->state }}">{{ $procedure_file->state }}</div>
+                        </div>
+                    </div>
+                  </div>
+              @endif
+
+            @empty
+                <div class="w-full border border-dashed border-[#cdd5de] rounded-[3px] flex py-1.5 justify-center text-[13px] leading-4 text-[#cdd5de]">
+                No hay archivos
+                </div>
+            @endforelse
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="space-y-6">
+      
     </div>
   </div>
 </div>
