9ab184784669cba295d270e9ea853981639f9370
Sandro Nilton <sandro91111@gmail.com>
2024-01-26 13:24:21 -0500

    change 52

--- resources/views/vendor/livewire-tables/components/table/th/plain.blade.php
+++ resources/views/vendor/livewire-tables/components/table/th/plain.blade.php
@@ -5,7 +5,7 @@
 @endphp
 
 @if ($theme === 'tailwind')
-    <th scope="col" {{ $attributes->merge(['class' => 'table-cell items-center px-4 pt-1.5 align-middle text-center md:text-center dark:bg-gray-800']) }}>{{ $slot }}</th>
+    <th scope="col" {{ $attributes->merge(['class' => 'table-cell items-center px-4 pt-1.5 align-middle text-white bg-[#4373C6] text-center md:text-center dark:bg-gray-800']) }}>{{ $slot }}</th>
 @elseif ($theme === 'bootstrap-4' || $theme === 'bootstrap-5')
     <th scope="col">{{ $slot }}</th>
 @endif
