0841fa370ba08fa6dbd74d911dfa469ee08de495
Guts <46543573+SandroNilton@users.noreply.github.com>
2024-05-09 09:16:23 -0500

    Update UpdateForm.php

--- app/Http/Livewire/Admin/Typeprocedures/UpdateForm.php
+++ app/Http/Livewire/Admin/Typeprocedures/UpdateForm.php
@@ -6,6 +6,7 @@ use App\Models\Procedure;
 use App\Models\Category;
 use App\Models\Area;
 use App\Models\Typeprocedure;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class UpdateForm extends Component
