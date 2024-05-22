ec15add948ed4667a4f3edba0599636b5f2419a3
Sandro Nilton <sandro91111@gmail.com>
2024-05-22 18:59:09 -0500

    save pro consult 13

--- resources/views/livewire/auth/consult.blade.php
+++ resources/views/livewire/auth/consult.blade.php
@@ -1,5 +1,8 @@
 <div class="w-full h-full p-4">
     <div class="flex flex-col h-full gap-4 w-full items-center">
+        <a href="{{ route('login') }}">
+            <x-primary-button type="button">Volver al Inicio</x-primary-button>
+          </a>
         <p class="mb-1 text-md font-semibold text-[rgb(17,24,39)] text-opacity-100">Consulta el estado de tu trámite</p>
     <div class="w-1/2">
         <form wire:submit.prevent="consult">
