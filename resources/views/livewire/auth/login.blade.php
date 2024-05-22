c2aeaaeb002f55013b3d40b9e7a5c1612186250a
Sandro Nilton <sandro91111@gmail.com>
2024-05-22 17:44:52 -0500

    save pro consult 2

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -6,7 +6,7 @@
     <div class="bg-white w-full md:max-w-md lg:max-w-full md:mw-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12 flex items-center justify-center overflow-hidden overflow-y-scroll">
       <div class="w-full h-100 px-4">
         <div class="justify-center flex -mt-50 mb-20">
-            <a href="{{ route('register') }}">
+            <a href="{{ route('consult') }}">
                 <img src="https://www.muniranrahirca.gob.pe/img/consulta_linea.png" width="200" alt="">
             </a>
         </div>
