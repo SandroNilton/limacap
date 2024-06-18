403614dfcf695ed97b81288e5cfa2bfcfc5b3406
Guts <46543573+SandroNilton@users.noreply.github.com>
2024-06-18 09:31:26 -0500

    Update consult.blade.php

--- resources/views/livewire/auth/consult.blade.php
+++ resources/views/livewire/auth/consult.blade.php
@@ -4,7 +4,7 @@
             <x-primary-button type="button">Volver al Inicio</x-primary-button>
           </a>
         <p class="mb-1 text-md font-semibold text-[rgb(17,24,39)] text-opacity-100">Consulta el estado de tu trámite</p>
-    <div class="w-1/2" >
+    <div class="flex flex-col h-full gap-4 items-center" >
         <form wire:submit.prevent="consult" class="w-1/2">
             <div class="mb-3">
               <x-text-input wire:model="code" type="text" name="code" placeholder="Codigo"/>
