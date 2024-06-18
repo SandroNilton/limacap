b75407f3514edcda70562b02638e2732cfaa286e
Guts <46543573+SandroNilton@users.noreply.github.com>
2024-06-18 09:16:13 -0500

    Update login.blade.php

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -5,7 +5,7 @@
     </div>
     <div class="bg-white w-full md:max-w-md lg:max-w-full md:mw-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12 flex items-center justify-center overflow-hidden overflow-y-scroll">
       <div class="w-full h-100 px-4">
-        <div class="justify-center flex -mt-50 mb-20">
+        <div class="justify-center flex -mt-50 mb-10">
             <img src="https://i.postimg.cc/PqDTPv8d/logo-niubiz-removebg-preview-3.png" width="220" alt="">
         </div>
         <div class="justify-center flex">
